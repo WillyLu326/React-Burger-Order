d0bed2d1c2eaf010d33061d7059a7398936eb2aa
WillyLu326 <luzh0326@gmail.com>
2017-11-01 19:23:02 -0400

    add BuildControls into BurgerBuilder component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import Aux from './../../hoc/Aux';
 import Burger from './../../components/Burger/Burger';
+import BuildControls from './../../components/BuildControls/BuildControls';
 
 class BurgerBuilder extends React.Component {
 
 	state = {
-    ingredients: {
-      salad: 1,
-      cheese: 1,
-      meat: 2,
-      bacon: 2
-    }
+		ingredients: {
+			salad: 1,
+			cheese: 1,
+			meat: 2,
+			bacon: 2
+		}
 	}
-	
+
 	render() {
 		return (
 			<Aux>
-				<Burger ingredients={this.state.ingredients}/>
+				<Burger ingredients={this.state.ingredients} />
+				<BuildControls ingredients={Object.keys(this.state.ingredients)} />
 			</Aux>
-    );
+		);
 	}
 }
 
