cfd43ad9068ee168386a90c7b98b2117af6c4125
WillyLu326 <luzh0326@gmail.com>
2017-11-01 19:16:34 -0400

    implement BuildControl component

--- src/components/BuildControls/BuildControl/BuildControl.js
+++ src/components/BuildControls/BuildControl/BuildControl.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+import ContentRemove from 'material-ui/svg-icons/content/remove';
 
 import classes from './BuildControl.css';
 
 const BuildControl = (props) => {
   return (
     <div className={classes.BuildControl}>
-
+      <div className={classes.ingredient}>{props.ingredient}</div>
+      <div className={classes.fab}>
+        <FloatingActionButton mini={true}>
+          <ContentAdd />
+        </FloatingActionButton>
+      </div>
+      <div className={classes.fab}>
+        <FloatingActionButton mini={true} secondary={true}>
+          <ContentRemove />
+        </FloatingActionButton>
+      </div>
     </div>
   );
 }
