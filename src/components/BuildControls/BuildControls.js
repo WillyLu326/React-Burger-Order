410106849b92588a57e8401bdab57df49fc4ee40
WillyLu326 <luzh0326@gmail.com>
2017-11-01 19:18:30 -0400

    implement BuildControls component

--- src/components/BuildControls/BuildControls.js
+++ src/components/BuildControls/BuildControls.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Aux from './../../hoc/Aux';
+import BuildControl from './BuildControl/BuildControl';
 
 const BuildControls = (props) => {
   return (
     <Aux>
-      
+      {
+        props.ingredients.map((ingredient, index) => {
+          return <BuildControl ingredient={ingredient} key={index} />
+        })
+      }
     </Aux>
   );
 }
