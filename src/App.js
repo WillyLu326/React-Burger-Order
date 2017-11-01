53dfddcaf2a6e84e5394a66863ec1d0fec2f3b89
WillyLu326 <luzh0326@gmail.com>
2017-10-31 23:32:50 -0400

    add layout into App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        This is willy lu
-      </div>
+      <Layout>
+        ddd
+      </Layout>
     );
   }
 }
