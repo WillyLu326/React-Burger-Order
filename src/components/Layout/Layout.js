e5d7cf49949a506acc5b3e23b3e06277d7285207
WillyLu326 <luzh0326@gmail.com>
2017-10-31 23:43:03 -0400

    install css-loader

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Aux from './../../hoc/Aux';
 import AppBar from 'material-ui/AppBar';
+import classes from './Layout.css';
 
 const Layout = (props) => {
 	return (
 		<Aux>
 			<AppBar title="Burger Order" />
-			<main>
-				props.children
+			<main className={classes.Content}>
+				{props.children}
 			</main>
 		</Aux>
 		
