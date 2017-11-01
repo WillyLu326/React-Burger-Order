93c4171fb84a5b52a4b9479494d475f5572f11f2
WillyLu326 <luzh0326@gmail.com>
2017-11-01 00:22:01 -0400

    create state

--- src/App.js
+++ src/App.js
@@ -4,11 +4,21 @@ import Layout from './components/Layout/Layout';
 import Burger from './components/Burger/Burger';
 
 class App extends Component {
+
+  state = {
+    ingredients: {
+      salad: 1,
+      cheese: 1,
+      meat: 2,
+      bacon: 2
+    }
+  }
+
   render() {
     return (
       <MuiThemeProvider>
         <Layout>
-          <Burger />
+          <Burger ingredients={this.state.ingredients}/>
         </Layout>
       </MuiThemeProvider>
     );
