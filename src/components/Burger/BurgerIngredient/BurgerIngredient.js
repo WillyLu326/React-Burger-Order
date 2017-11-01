ad4c1d5a4267e63c019d7d06b76776c2e1495188
WillyLu326 <luzh0326@gmail.com>
2017-10-31 23:53:07 -0400

    create BurgerIngredient component

---
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class BurgerIngredient extends React.Component {
+	render() {
+		let ingredient = null;
+
+
+		return ingredient;
+	}
+}
+
+export default BurgerIngredient;
