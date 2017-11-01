8db7ce2e9f103e37ddf39a6fc9d2bafe96813683
WillyLu326 <luzh0326@gmail.com>
2017-11-01 00:04:47 -0400

    create dummy burger

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Aux from './../../hoc/Aux';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const Burger = () => {
 	return (
 		<Aux>
-			
+			<BurgerIngredient type="bread-top" />
+			<BurgerIngredient type="cheese" />
+			<BurgerIngredient type="meat" />
+			<BurgerIngredient type="salad" />
+			<BurgerIngredient type="bacon" />
+			<BurgerIngredient type="bread-bottom" />
 		</Aux>
 	);
 }
