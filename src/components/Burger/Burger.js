cda8e6080af11415ad106495f1f5d9d848a5272c
WillyLu326 <luzh0326@gmail.com>
2017-11-01 00:27:20 -0400

    create dynamic burger

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,14 +3,27 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 import classes from './Burger.css';
 
-const Burger = () => {
+const Burger = (props) => {
+
+	let convertedIngredients = Object.keys(props.ingredients)
+		.map(ingredient => {
+			return [...Array(props.ingredients[ingredient]).fill(ingredient)]
+				.map((_, index) => {
+					return (
+						<BurgerIngredient type={ingredient} key={ingredient + index}/>
+					);
+				})
+		})
+		.reduce((initArray, curtArray) => initArray = initArray.concat(curtArray), []);
+
+	if (convertedIngredients.length === 0) {
+		convertedIngredients = <p>Plz Add Some Ingredients</p>
+	}
+
 	return (
 		<div className={classes.Burger}>
 			<BurgerIngredient type="bread-top" />
-			<BurgerIngredient type="cheese" />
-			<BurgerIngredient type="meat" />
-			<BurgerIngredient type="salad" />
-			<BurgerIngredient type="bacon" />
+			{convertedIngredients}
 			<BurgerIngredient type="bread-bottom" />
 		</div>
 	);
