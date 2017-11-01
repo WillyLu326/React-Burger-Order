97d94f87a83d9a1b4303c0126be1c7684f4401b6
WillyLu326 <luzh0326@gmail.com>
2017-11-01 19:38:06 -0400

    style the page

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Aux from './../../hoc/Aux';
 import Burger from './../../components/Burger/Burger';
 import BuildControls from './../../components/BuildControls/BuildControls';
+import Paper from 'material-ui/Paper';
 
 class BurgerBuilder extends React.Component {
 
@@ -14,10 +15,22 @@ class BurgerBuilder extends React.Component {
 		}
 	}
 
+	paperStyle = {
+		position: 'relative',
+		width: '90%',
+		height: 'auto',
+		left: '5%',
+		right: '5%',
+		backgroundColor: '#6AB3F4',
+		margin: '0px 0px 16px 0px'
+	}
+
 	render() {
 		return (
 			<Aux>
-				<Burger ingredients={this.state.ingredients} />
+				<Paper style={this.paperStyle} zDepth={2}>
+					<Burger ingredients={this.state.ingredients} />
+				</Paper>
 				<BuildControls ingredients={Object.keys(this.state.ingredients)} />
 			</Aux>
 		);
