961477026d06b2e50da238d23a8a711f758cf51e
WillyLu326 <luzh0326@gmail.com>
2017-10-31 23:59:37 -0400

    create BurgerIngredient.js

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Aux from './../../hoc/Aux';
+
+const Burger = () => {
+	return (
+		<Aux>
+			
+		</Aux>
+	);
+}
+
+export default Burger;
--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -4,6 +4,28 @@ class BurgerIngredient extends React.Component {
 	render() {
 		let ingredient = null;
 
+		switch (this.props.type) {
+			case 'bread-top':
+				ingredient = <div></div>;
+				break;
+			case 'bread-bottom':
+				ingredient = <div></div>;
+				break;
+			case 'salad':
+				ingredient = <div></div>;
+				break;
+			case 'cheese':
+				ingredient = <div></div>;
+				break;
+			case 'bacon':
+				ingredient = <div></div>;
+				break;
+			case 'meat':
+				ingredient = <div></div>;
+				break;
+			default:
+			ingredient = null;
+		}
 
 		return ingredient;
 	}
