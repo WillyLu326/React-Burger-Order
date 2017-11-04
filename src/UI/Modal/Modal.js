68741c4ed603aedb11a763a621df907fc1661843
WillyLu326 <luzh0326@gmail.com>
2017-11-03 23:35:59 -0400

    convert Modal from class component to Component component

--- src/UI/Modal/Modal.js
+++ src/UI/Modal/Modal.js
@@ -2,34 +2,36 @@ import React from 'react';
 import Dialog from 'material-ui/Dialog';
 import RaisedButton from 'material-ui/RaisedButton';
 
-class Modal extends React.Component {
-  
-  render () {
-    const actions = [
-      <RaisedButton 
-        label="Cancel"
-        primary={true}
-        onClick={this.props.open}
-      />,
-      <RaisedButton 
-        label="Check out"
-        secondary={true}
-        onClick={this.props.close}
-      />,
-    ]
+import classes from './Modal.css';
 
-    return (
-      <div>
-        <Dialog
-          title="Burger Order Summary"
-          actions={actions}
-          modal={true}
-          open={this.props.status}
-        >
-        </Dialog>
-      </div>
-    );
-  }
+const Modal = (props) => {
+
+  const actions = [
+    <RaisedButton 
+      label="Cancel"
+      secondary={true}
+      className={classes.btn}
+      onClick={props.close}
+    />,
+    <RaisedButton 
+      label="Check out"
+      primary={true}
+      className={classes.btn}
+      onClick={props.close}
+    />
+  ];
+
+  return (
+    <div>
+      <Dialog
+        title="Burger Order Summary"
+        actions={actions}
+        modal={true}
+        open={props.status}
+      >
+      </Dialog>
+    </div>
+  );
 }
 
 export default Modal;
