f85c777c35ba18438df4a9d9a0b7859b08086452
WillyLu326 <luzh0326@gmail.com>
2017-11-04 00:10:44 -0400

    add badge

--- src/components/OrderSummary/OrderSummary.js
+++ src/components/OrderSummary/OrderSummary.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { List, ListItem } from 'material-ui/List';
+import Badge from 'material-ui/Badge';
 
 const OrderSummary = (props) => {
   const listItems = Object.keys(props.ingredients)
     .map((ingredient, index) => {
       return (
-        <ListItem key={index} primaryText={ingredient.toUpperCase()} rightIcon={<div>{props.ingredients[ingredient]}</div>} />
+        <ListItem 
+          key={index} 
+          primaryText={ingredient.toUpperCase()} 
+          rightIcon={<Badge badgeContent={props.ingredients[ingredient]} primary={true} />} />
       );
-    }) 
+    });
 
   return (
     <List>
