0e33cfe4e89517583b1499cb250c502a0cca50a8
WillyLu326 <luzh0326@gmail.com>
2017-11-09 19:26:51 -0500

    convert Layout into class Componnet

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import Aux from './../../hoc/Aux';
 import AppBar from 'material-ui/AppBar';
+import SideMenu from './../../UI/SideMenu/SideMenu';
+
 import classes from './Layout.css';
 
-const Layout = (props) => {
-	return (
-		<Aux>
-			<AppBar title="Burger Order" />
-			<main className={classes.Content}>
-				{props.children}
-			</main>
-		</Aux>
-		
-	);
+class Layout extends React.Component {
+
+	state = {
+		drawerOpen: false
+	}
+
+	render () {
+		return (
+			<Aux>
+				<AppBar title="Burger Order" />
+				<SideMenu open={this.state.drawerOpen} />
+				<main className={classes.Content}>
+					{this.props.children}
+				</main>
+			</Aux>
+		);
+	}
 };
 
 export default Layout;
