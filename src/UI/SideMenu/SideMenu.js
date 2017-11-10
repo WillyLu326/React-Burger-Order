01a9f8a296f7b1c457e9c33de04f574fefaf5a42
WillyLu326 <luzh0326@gmail.com>
2017-11-09 19:23:44 -0500

    add menuItem into Drawer

--- src/UI/SideMenu/SideMenu.js
+++ src/UI/SideMenu/SideMenu.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Drawer } from 'material-ui/Drawer';
+import { MenuItem } from 'material-ui/MenuItem';
 
 const SideMenu = (props) => {
   return (
-
+    <Drawer>
+      <MenuItem>Menu</MenuItem>
+      <MenuItem>Burger</MenuItem>
+    </Drawer>
   );
 }
 
